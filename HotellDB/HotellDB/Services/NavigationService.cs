8596948f6d4f64b240e9cfed9db14d9e8231a1e6
Nikita <nikitka.fomin.06@inbox.ru>
2026-03-31 13:00:57 +0300

    Add files via upload

---
+++ HotellDB/HotellDB/Services/NavigationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellDB.Services
+{
+    public static class NavigationService
+    {
+        public static event Action<object> NavigateTo;
+
+        public static void Navigate(object viewModel)
+        {
+            NavigateTo?.Invoke(viewModel);
+        }
+    }
+}
